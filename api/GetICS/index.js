96ae57bcaff4e92067ebeb6f8540b9027f261d3d
Tom Plant <21111317+pl4nty@users.noreply.github.com>
2021-10-24 14:13:24 +1100

    Change to custom ICS timezone

--- api/GetICS/index.js
+++ api/GetICS/index.js
@@ -7,16 +7,21 @@ const TIMETABLE_JSON = 'https://raw.githubusercontent.com/pl4nty/anutimetable/ma
 const tz = 'Australia/Canberra'
 const days = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA']
 
-function timesToArray(date, timeString) {
+function timesToArray(date, timeString, context) {
     const times = timeString.split(':')
     date.setHours(times[0])
     date.setMinutes(times[1])
     date.setSeconds(0)
-    return fns.format(date, 'y,M,d,H,m,s', { timeZone: tz}).split(',').map(x => parseInt(x))
+    context.log.error('date:',date)
+    let date2 = fns.format(date, 'y,M,d,H,m,s', { timeZone: tz}).split(',').map(x => parseInt(x))
+    context.log.error('date2:',date2)
+    return date2
 }
 
 // eg ?COMP2310_S2=COMP2310_S2-LecA/01,COMP2310_S2-LecB/01
 module.exports = async function (context, req) {
+    context.log.info(`Running in node ${process.version}`)
+
     const courseCodes = Object.keys(req.query)
     
     // require Intl module for timezone handling
@@ -59,10 +64,10 @@ module.exports = async function (context, req) {
                     // Static Web App Functions don't support WEBSITE_TIME_ZONE and default to UTC, so manually handle timezones
                     // Days from start of year until first Monday - aka Week 0
                     let yearStart = fns.utcToZonedTime(new Date(), tz)
-                    context.warn(yearStart)
-                    context.warn(new Date())
+                    context.log.warn('yr:',yearStart)
+                    context.log.warn('date:',new Date())
                     yearStart.setMonth(0,1)
-                    context.warn(yearStart)
+                    context.log.warn('start:',yearStart)
 
                     const year = yearStart.getFullYear()
                     const dayOffset = (8 - yearStart.getDay()) % 7
@@ -74,22 +79,24 @@ module.exports = async function (context, req) {
                         
                         const day = dayOffset + 7*(interval[0]-2) + parseInt(session.day) + 1
                         
-                        context.warn(yearStart)
                         let startDay = fns.utcToZonedTime(new Date(yearStart.getTime()), tz)
-                        context.warn(startDay)
+                        context.log.warn('startday:',startDay)
                         startDay.setDate(day)
-                        context.warn(startDay)
-                        const weekday = days[startDay.getDay()]-1 // assumes no multi-day events
+                        context.log.warn('startdayset:',startDay)
+                        const weekday = days[startDay.getDay()] // assumes no multi-day events
+
+                        console.log(process.version)
 
                         events.push({
-                            start: timesToArray(startDay, session.start),
-                            end: timesToArray(startDay, session.finish),
+                            start: timesToArray(startDay, session.start, context),
+                            startOutputType: 'local',
+                            end: timesToArray(startDay, session.finish, context),
                             title: `${session.module} ${session.activity} ${parseInt(session.occurrence)}`,
                             description: `${session.activity} ${parseInt(session.occurrence)}`,
                             location: session.location,
                             url: session.locationID,
                             productId: 'anucssa/timetable',
-                            uid: session.name+weeks,
+                            uid: session.name+weeks.replace('\u2011','-'),
                             recurrenceRule: `FREQ=WEEKLY;BYDAY=${weekday};INTERVAL=1;COUNT=${repetitions}`,
                             calName: `ANU Timetable ${year} ${session.session}`
                         })
@@ -100,7 +107,32 @@ module.exports = async function (context, req) {
     }
 
     if (events.length !== 0) {
-        const { value, error } = ics.createEvents(events)
+        let { value, error } = await ics.createEvents(events)
+
+        // Cursed timezone magic
+        // Breaks if Canberra TZ changes
+        value = value.replace(/DTSTART/g, 'DTSTART;TZID="Australia/Canberra"')
+        value = value.replace(/DTEND/g, 'DTEND;TZID="Australia/Canberra"')
+        value = value.replace(/BEGIN:VEVENT/,`BEGIN:VTIMEZONE
+TZID:Australia/Canberra
+X-LIC-LOCATION:Australia/Canberra
+BEGIN:DAYLIGHT
+TZOFFSETFROM:+1000
+TZOFFSETTO:+1100
+TZNAME:EST
+DTSTART:19701025T020000
+RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU
+END:DAYLIGHT
+BEGIN:STANDARD
+TZOFFSETFROM:+1000
+TZOFFSETTO:+1000
+TZNAME:EST
+DTSTART:19700329T020000
+RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU
+END:STANDARD
+END:VTIMEZONE
+BEGIN:VEVENT`)
+
         context.res = {
             status: 200,
             headers: {'Content-Type': 'text/calendar'},
