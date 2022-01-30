e75a590f5be441649bba4a5b1192c6ebc683c7ee
Tom Plant <21111317+pl4nty@users.noreply.github.com>
2022-01-30 22:36:22 +1100

    fix: multiselect modal race condition

--- src/SelectModal.js
+++ src/SelectModal.js
@@ -2,34 +2,36 @@ import { Modal, Form, Button } from 'react-bootstrap'
 import { useState } from 'react'
 
 const SelectModal = ({ visible, title, label, options, value, multiple, inline, onChange, onHide, ...props }) => {
-  const getValues = options => [].slice.call(options).map(item => +item.value)
-  const hide = () => onHide(getValues(selected))
+  const hide = () => onHide(selected)
 
   // if single-select, return selection as singleton array for consistency
   const singleSelect = multiple ? undefined : e => {
-    setSelected([e.target])
-    onChange(getValues(selected))
+    setSelected([+e.target.value])
+    onChange([+e.target.value])
   }
 
   // if multi-select, prevent clearing existing selections
   // allows multiple selections without pressing ctrl
   const multiSelect = !multiple ? undefined : e => {
     e.preventDefault()
-    setSelected(e.target.parentNode.selectedOptions)
+    const newSelect = +e.target.value
+    const index = selected.indexOf(newSelect)
+    const newSelection = e.target.selected ? [...selected.slice(0, index), ...selected.slice(index+1)] : [...selected, newSelect]
+    setSelected(newSelection)
+    onChange(newSelection)
     e.target.selected = !e.target.selected
-    onChange(getValues(selected))
   }
 
   const [selected, setSelected] = useState([])
 
-  return <Modal size='xl' centered show={visible} onHide={hide} {...props}>
+  return <Modal size="xl" centered show={visible} onHide={hide} {...props}>
     <Modal.Header closeButton>
       <Modal.Title>{title}</Modal.Title>
     </Modal.Header>
     <Modal.Body>
       <Form inline={inline}>
         <Form.Label>{label}</Form.Label>
-        <Form.Control as='select' custom multiple={multiple} defaultValue={value} onChange={singleSelect}>
+        <Form.Control as="select" custom multiple={multiple} defaultValue={value} onChange={singleSelect}>
           {options.map((option, index) => <option key={index} value={index} onMouseDown={multiSelect}>
             {option}
           </option>)}
@@ -37,7 +39,7 @@ const SelectModal = ({ visible, title, label, options, value, multiple, inline, 
       </Form>
     </Modal.Body>
     <Modal.Footer>
-      <Button variant='secondary' onClick={hide}>
+      <Button variant="secondary" onClick={hide}>
         Close
       </Button>
     </Modal.Footer>
