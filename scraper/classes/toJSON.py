67bb1cf5790402d58aecd4cc913d09d0546c93f1
CraftyDH <23249291+CraftyDH@users.noreply.github.com>
2022-02-11 11:40:03 +1100

    Remove some unused properties

--- scraper/classes/toJSON.py
+++ scraper/classes/toJSON.py
@@ -6,6 +6,7 @@ from calendar import timegm
 
 import re
 
+
 def removeEmptyCourses(course, acc):
     if len(course.classes) == 0:
         acc.append(course.id)
@@ -13,25 +14,30 @@ def removeEmptyCourses(course, acc):
     return True
 
 # Converts a list of objects to a list of property dicts
+
+
 def courseToDicts(course):
     course.classes = [x.__dict__ for x in course.classes]
     return course.__dict__
 
+
 def formatCourses(courses: List[Course]):
     acc = []
-    dicts = [courseToDicts(x) for x in list(filter(lambda x: removeEmptyCourses(x, acc), courses))]
+    dicts = [courseToDicts(x) for x in list(
+        filter(lambda x: removeEmptyCourses(x, acc), courses))]
     print(f"{len(acc)} courses had no events: {acc}")
-    
+
     obj = {}
     for x in dicts:
         obj[x['id']] = x
-    data = json.JSONEncoder(indent=1).encode(obj)
+    data = json.dumps(obj, separators=(',', ':'))
     f = open("timetable.json", "w+")
     f.write(data)
     f.close()
 
+
 def clockToDecimal(s: str):
-    l = re.split(r":",s)
+    l = re.split(r":", s)
     h = int(l[0])
     m = int(l[1])/60
     if m == 0:
