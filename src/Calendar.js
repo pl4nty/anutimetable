75c3031c526040ad5387de0af7d4f48f3f8023db
CraftyDH <23249291+CraftyDH@users.noreply.github.com>
2022-02-12 10:41:56 +1100

    fix: HTML violation warning for nested a tags in events (#131)

--- src/Calendar.js
+++ src/Calendar.js
@@ -39,11 +39,11 @@ const formatEventContent = ({ selectOccurrence, resetOccurrence, hideOccurrence 
       ? <button className='choose-button' onClick={() => dispatch(selectOccurrence)}>Choose</button>
       : <button className='choose-button' onClick={() => dispatch(resetOccurrence)}>Reset</button>
   return <>
-    <div className='hide-button' title='Hide this event' onClick={() => dispatch(hideOccurrence)}>
+    <div className="hide-button" title="Hide this event" onClick={() => dispatch(hideOccurrence)}>
       <svg viewBox="0 0 24 24"><path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"></path></svg>
     </div>
     <p>{event.title}</p>
-    <p>{locationLine}</p>
+    <object>{locationLine}</object>
     <p>{button}</p>
   </>
 }
