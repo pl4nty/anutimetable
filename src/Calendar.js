a9a84ca8207a623e28ac05e45f456a54a564a421
CraftyDH <23249291+CraftyDH@users.noreply.github.com>
2022-02-18 13:27:15 +1100

    Make each line non wrapping

--- src/Calendar.js
+++ src/Calendar.js
@@ -42,7 +42,7 @@ const formatEventContent = ({ setSpecifiedOccurrences, setHiddenEvents }, { even
       <svg viewBox="0 0 24 24"><path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"></path></svg>
     </div>
     <p>{event.title}</p>
-    <object>{locationLine}</object>
+    <p>{locationLine}</p>
     <p>{button}</p>
   </>
 }
