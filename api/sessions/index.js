3ad076f16d8d66242bfac45dd9ef0b32aaf8f625
CraftyDH <23249291+CraftyDH@users.noreply.github.com>
2023-01-04 18:34:07 +1100

    Add 2023 timetable data (#206)
    Co-authored-by: Daniel Herald <u7480080@anu.edu.au>

--- api/sessions/index.js
+++ api/sessions/index.js
@@ -1,7 +1,9 @@
 module.exports = async function (context, req) {
     context.res = {
         body: {
-            2022: ['S1','S2','X1','X2','X3','X4']
+            2021: ['S1','S2','X1','X2','X3','X4'],
+            2022: ['S1','S2','X1','X2','X3','X4'],
+            2023: ['S1','S2','X1','X2','X3','X4'],
         }
     }
     
